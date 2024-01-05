fd4427ac871c37afda988145b7c12482507a801e
romina gonzalez <rominagonzalez.design@gmail.com>
2024-01-05 19:06:23 -0300

    sweet alert, seteo de info sin resolver aun

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 
-
-//variables y arrays
-
-//fechas para el calendario
+//variables y arrays 
 const MONTH_NAMES = ["ENERO", "FEBRERO", "MARZO", "ABRIL", "MAYO", "JUNIO", "JULIO", "AGOSTO", "SEMPTIEMBRE", "OCTUBRE", "NOVIEMBRE", "DICIEMBRE"]
 const DIAS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
 const DIAS_OBJETO = [];
+
+let actualMonth = new Date();
+let actualYear = new Date();
+let monthName = MONTH_NAMES[actualMonth.getMonth()];
+
+//clase y objeto para almacenar tareas
 
 class Dia {
     constructor(numero) {
@@ -19,12 +22,6 @@ class Dia {
 }
 DIAS.forEach(numero => DIAS_OBJETO.push(new Dia(numero)));
 
-
-let actualMonth = new Date();
-let actualYear = new Date();
-let monthName = MONTH_NAMES[actualMonth.getMonth()];
-
-
 // inicio
 function greeting() {
     alert("¡Bienvenido a tu agenda personal!");
@@ -34,33 +31,40 @@ greeting();
 // MES Y AÑO ACTUAL CALENDARIO
 const CURRENT_DATE = document.getElementById("currentDate");
 CURRENT_DATE.innerHTML = `<p>
-                            ${ monthName }
-                            ${ actualYear.getFullYear()}
+                            ${monthName}
+                            ${actualYear.getFullYear()}
                         </p>`;
 
-// imprimir los dias del mes - por cada dia, modal popup
+// imprimir los dias del mes - por cada dia
 
 const DAYS_WEEK = document.getElementById("daysWeek");
-const CLOSE_MODAL = document.getElementById("cerrarModal");
-const NEW_TASK = document.getElementById("newTask");
+const DAYS_MONTH = document.getElementById("daysMonth");
+
 DIAS_OBJETO.forEach(dia => {
     const boton = document.createElement("button");
     boton.className = "daysButton";
     boton.innerHTML = `${dia.numero}`;
-    boton.addEventListener("click", dia => {
-        modalPopUp.showModal();
-        // numero.agregarTarea(asd, ads)
-    }) 
-    //clase objetos para almacenar tareas
-
-
-    //cerrar modal
-    CLOSE_MODAL.addEventListener("click", () => {
-        modalPopUp.close();
+    boton.addEventListener("click", () => {
+        Swal.fire({
+            title: "Tareas de la fecha",
+            html: `
+            <form>
+                <label for="inputTime">Selecciona la hora:</label>
+                <input type="time" id="inputTime" name="inputTime">
+                <input type="text" id="inputTask" placeholder="Descripcion">
+                <input type="text" id="inputfecha" placeholder="dia">
+            </form>
+            `,
+            confirmButtonText: "Agendar",
+            showCancelButton: true,
+            cancelButtonText: "Volver"
+        })
     })
-
-    DAYS_WEEK.appendChild(boton);
+    DAYS_MONTH.appendChild(boton);
 });
+// const inputTime = document.getElementById("inputTime").value;
+// const inputTask = document.getElementById("inputTask").value;
+// const inputFecha = document.getElementById("inputFecha").value;
 
 
 //colorTheme
@@ -68,7 +72,7 @@ const colorTheme = document.getElementById("modeTheme");
 
 colorTheme.addEventListener("click", () => {
     document.body.classList.toggle("orange");
-    if(document.body.classList.contains("orange")){
+    if (document.body.classList.contains("orange")) {
         localStorage.setItem("mode", "orange");
     } else {
         localStorage.setItem("mode", "pink");
